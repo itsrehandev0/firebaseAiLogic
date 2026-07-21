1144b6b31eb13cccb05ed1c6395642ab44624717
itsrehandev0 <itsrehandev@gmail.com>
2026-07-21 10:19:43 -0700

    design and package set up

---
+++ lib/screens/home/app_drawer.dart
@@ -0,0 +1,59 @@
+import 'package:firebase_ai_logic/utilities/app_color.dart';
+import 'package:firebase_ai_logic/utilities/app_images.dart';
+import 'package:firebase_ai_logic/widgets/primary_tile.dart';
+import 'package:flutter/material.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: AppColor.whiteColor,
+      child: SafeArea(
+        child: Column(
+          children: [_DrawerHeader(), SizedBox(height: 8), _AppFeatures()],
+        ),
+      ),
+    );
+  }
+}
+
+class _AppFeatures extends StatelessWidget {
+  const _AppFeatures();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        PrimaryTile(
+          title: 'Food',
+          subTitle: 'Generate food reciepe',
+          icon: Icons.abc_sharp,
+        ),
+      ],
+    );
+  }
+}
+
+class _DrawerHeader extends StatelessWidget {
+  const _DrawerHeader();
+
+  @override
+  Widget build(BuildContext context) {
+    return DrawerHeader(
+      child: Column(
+        children: [
+          Image.asset(AppImages.appLogo, height: 100, width: 100),
+          SizedBox(height: 8),
+          Text(
+            'FIREBASE AI',
+            style: Theme.of(
+              context,
+            ).textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.w900),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/primary_card.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_ai_logic/utilities/app_color.dart';
+import 'package:firebase_ai_logic/utilities/constant.dart';
+import 'package:flutter/material.dart';
+
+class PrimaryCard extends StatelessWidget {
+  const PrimaryCard({
+    super.key,
+    this.borderColor,
+    this.borderRadius = kBorderRadius,
+    this.width,
+    this.backgroundColor,
+    this.padding,
+    this.margin,
+    required this.child,
+  });
+  final Color? borderColor;
+  final double borderRadius;
+  final double? width;
+  final Color? backgroundColor;
+  final EdgeInsets? padding;
+  final EdgeInsets? margin;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: padding ?? EdgeInsets.zero,
+      margin: margin ?? EdgeInsets.zero,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: borderColor ?? AppColor.cardColor,
+          width: width ?? 1,
+        ),
+        borderRadius: BorderRadius.circular(borderRadius),
+        color: backgroundColor,
+      ),
+      child: child,
+    );
+  }
+}
